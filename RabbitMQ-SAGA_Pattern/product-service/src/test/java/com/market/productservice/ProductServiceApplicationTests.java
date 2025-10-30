265b47bb0acd4c07c56816cd866d6f139b632c55
rudcjs <rudcjs335@gmail.com>
2025-10-30 14:46:43 +0900

    init commit

---
+++ RabbitMQ-SAGA_Pattern/product-service/src/test/java/com/market/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
