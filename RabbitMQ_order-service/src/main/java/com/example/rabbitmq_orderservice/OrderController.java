8c9d1985f99226999cbd2915d1214f50cdfd5e31
rudcjs <rudcjs335@gmail.com>
2025-10-30 11:43:33 +0900

    간단한 메시지 queue에 보내보기

---
+++ RabbitMQ_order-service/src/main/java/com/example/rabbitmq_orderservice/OrderController.java
@@ -0,0 +1,20 @@
+package com.example.rabbitmq_orderservice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping("{id}")
+    public String getOrder(@PathVariable String id) {
+        orderService.createOrder(id);
+        return "Get Order Complete";
+    }
+}
