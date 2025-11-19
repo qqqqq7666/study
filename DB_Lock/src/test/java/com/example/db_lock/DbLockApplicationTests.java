f63c5e33c883245684ef2f1ae79c120cd7a8460b
rudcjs <rudcjs335@gmail.com>
2025-11-19 12:22:22 +0900

    DB Lock 실습 init commit

---
+++ DB_Lock/src/test/java/com/example/db_lock/DbLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db_lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
