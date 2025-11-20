9800a92fe2000505653aceb2846755e1ba37a248
rudcjs <rudcjs335@gmail.com>
2025-11-20 15:13:21 +0900

    데드 락 실습

--- DB_Lock/src/main/java/com/example/db_lock/item/ItemService.java
+++ DB_Lock/src/main/java/com/example/db_lock/item/ItemService.java
@@ -2,16 +2,18 @@ package com.example.db_lock.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository repository;
+    private final ItemRepository itemRepository;
 
     @Transactional
-    public void save(Item item) {
-        repository.save(item);
+    public Item save(Item item) {
+        return repository.save(item);
     }
 
     @Transactional
@@ -23,5 +25,22 @@ public class ItemService {
     @Transactional
     public Item findById(Long id) {
         return repository.findByIdWithLock(id);
+    }
+
+    @Transactional(timeout = 1, isolation = Isolation.SERIALIZABLE)
+    public void updateItemQuantity(Long id1, Long id2) {
+        Item item1 = itemRepository.findByIdWithLock(id1);
+        item1.setQuantity(item1.getQuantity() + 10);
+        itemRepository.save(item1);
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Item item2 = itemRepository.findByIdWithLock(id2);
+        item2.setQuantity(item2.getQuantity() + 10);
+        itemRepository.save(item2);
     }
 }
--- DB_Lock/src/test/java/com/example/db_lock/ItemServiceTest.java
+++ DB_Lock/src/test/java/com/example/db_lock/ItemServiceTest.java
@@ -9,11 +9,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 public class ItemServiceTest {
     private static final Logger log = LoggerFactory.getLogger(ItemServiceTest.class);
-    @Autowired
-    private ItemRepository itemRepository;
     @Autowired
     private ItemService itemService;
 
@@ -46,5 +49,67 @@ public class ItemServiceTest {
 
         Item updatedItem = itemService.findById(item.getId());
         log.info("최종 아이템 수량: {}", updatedItem.getQuantity());
+    }
+
+    @Test
+    void deadlockTest() throws InterruptedException {
+        Item item1 = Item.builder()
+                .name("test item")
+                .quantity(100)
+                .build();
+        Item savedItem1 = itemService.save(item1);
+
+        Item item2 = Item.builder()
+                .name("test item")
+                .quantity(200)
+                .build();
+        Item savedItem2 = itemService.save(item2);
+
+        CountDownLatch latch = new CountDownLatch(1);
+        AtomicReference<Exception> exceptionThread = new AtomicReference<>();
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                log.info("thread1 start");
+                latch.await();
+                log.info("thread1: attempt item 1 -> item 2 update");
+                itemService.updateItemQuantity(savedItem1.getId(), savedItem2.getId());
+                log.info("thread1: update complete");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (Exception e) {
+                log.error("unknown error is occurred", e);
+                exceptionThread.set(e);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                log.info("thread2 start");
+                latch.await();
+                log.info("thread2: attempt item 1 -> item 2 update");
+                itemService.updateItemQuantity(savedItem1.getId(), savedItem2.getId());
+                log.info("thread2: update complete");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (Exception e) {
+                log.error("unknown error is occurred", e);
+                exceptionThread.set(e);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        latch.countDown();
+
+        thread1.join();
+        thread2.join();
+
+        assertThrows(Exception.class, () -> {
+            if(exceptionThread.get() != null) {
+                throw exceptionThread.get();
+            }
+        });
     }
 }
